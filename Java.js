d9f471ed16f3ce02d5c9190e9a1b6f3da36fd7ad
Abdulloh Mahmudjanov <mahmudjanovabdulloh358@gmail.com>
2025-03-05 14:22:07 +0300

    JS1 Darslik

---
+++ Java.js
@@ -0,0 +1,18 @@
+// ozgaruvchilar 3ta boladi 
+// let const var
+
+// ozgaruvchilar - malumaot saqlash uchun ishlatiladi.
+
+// let muhammadsodiq = "2008";
+// console.log(muhammadsodiq);
+
+let hello = "mening birinchi malumotim"
+hello = "muhammad"
+hello = "jigar"
+hello = "javohir"
+console.log(hello)
+
+let newStudentGroup = "Yaxshi"
+let message = "xabar"
+let Message = "Xabar"
+console.log(newStudentGroup);
